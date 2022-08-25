96fb40d85b49146da4bf11876bcb2c98c3528c0c
Willian Birollo <willianbirollo@outlook.com>
2022-08-25 10:58:51 -0300

    Other entities e second migration

---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string nome, string email, DateTime birthDate, double baseSalary, Department department, ICollection<SalesRecord> sales)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+            Sales = sales;
+        }
+
+        public void AddSales(SalesRecord salesRecord)
+        {
+            Sales.Add(salesRecord);
+        }
+
+        public void RemoveSales(SalesRecord salesRecord)
+        {
+            Sales.Remove(salesRecord);
+        }
+
+        public  double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(salesRecord => salesRecord.Date >= initial && salesRecord.Date <= final)
+                .Sum(salesRecord => salesRecord.Amount);
+        }
+    }
+}
