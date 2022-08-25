b78a567022a4d495104b7506b2f689f6cf251de7
Willian Birollo <willianbirollo@outlook.com>
2022-08-25 14:57:38 -0300

    Delete Seller

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -26,5 +26,17 @@ namespace SalesWebMVC.Services
             _context.Add(seller);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(seller => seller.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var seller = _context.Seller.Find(id);
+            _context.Seller.Remove(seller);
+            _context.SaveChanges();
+        }
     }
 }
