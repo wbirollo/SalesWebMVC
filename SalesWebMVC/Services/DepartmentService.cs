ed0845b38b2b9176aa4fe90308d03c168a991148
Willian Birollo <willianbirollo@outlook.com>
2022-08-31 11:24:26 -0300

    Asynchronous operations

--- SalesWebMVC/Services/DepartmentService.cs
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMVC.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
 using SalesWebMVC.Models;
 using System;
 using System.Collections.Generic;
@@ -15,9 +16,9 @@ namespace SalesWebMVC.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(department => department.Name).ToList();
+            return await _context.Department.OrderBy(department => department.Name).ToListAsync();
         }
     }
 }
